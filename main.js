59fddc03ab42037c5cc34a8cc51e158a54a56874
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-05-16 12:09:23 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const playSonidoPom = () => {
+  document.querySelector("#sonido_tecla_pom").play();
+};
+
+// document.querySelector(".tecla_pom").onclick = playSonidoPom;
+
+let listaDeTeclas = document.querySelectorAll(".tecla");
